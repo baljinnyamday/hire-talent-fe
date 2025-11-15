e0a31cc8f022d4233031fb7249ef46d4409e889a
Baljinnyam dayan <baljinnyam.dayan@gmail.com>
2025-11-15 15:00:18 +0000

    gg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,14 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
